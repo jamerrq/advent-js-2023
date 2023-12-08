a09ec3cc78a7b51a5a5d7b5fe8730484615b96b0
jamerrq <jamerrq@gmail.com>
2023-12-08 11:35:45 -0500

    ♻️ refactor solution for challenge 7

--- src/challenges/07.ts
+++ src/challenges/07.ts
@@ -1,20 +1,23 @@
 export function drawGift (size: number, symbol: string): string {
   const topLines = []
   const bottomLines = []
+  const symbolPattern = symbol.repeat(Math.max(0, size - 2))
   for (let i = 0; i < size - 1; ++i) {
     let line = ''
     if (i === 0) {
       line = '#'.repeat(size)
     } else {
-      line = `#${symbol.repeat(Math.max(0, size - 2))}#${symbol.repeat(i - 1)}#`
+      const symPtn = symbol.repeat(i - 1)
+      line = `#${symbolPattern}#${symPtn}#`
     }
     const spaces = size - i - 1
     topLines.push(`${' '.repeat(spaces)}${line}`)
     bottomLines.unshift(line)
   }
-  // console.log(topLines)
-  // console.log(bottomLines)
-  const middleLine = size > 1 ? `\n${'#'.repeat(size)}${symbol.repeat(Math.max(0, size - 2))}#\n` : '#'
+  const barrer = '#'.repeat(size)
+  const middleLine = size > 1
+    ? `\n${barrer}${symbolPattern}#\n`
+    : '#'
   const result = topLines.join('\n') + middleLine + bottomLines.join('\n')
   return result + '\n'
 }
