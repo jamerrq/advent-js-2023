a904519ea0cc4b3cdbfd9c50f2960e79c14eddf9
jamerrq <jamerrq@gmail.com>
2023-12-21 11:32:39 -0500

    🛷 refactor code for challenge 05

--- src/challenges/05.ts
+++ src/challenges/05.ts
@@ -2,29 +2,30 @@ export function cyberReindeer (road: string, time: number): string[] {
   // Code here
   const ans = [road]
   let roadCopy = [...road.replace('S', '.')]
-  let currentTime = 1
+  let currTime = 1
   let waitTime = 0
-  while (currentTime < time) {
-    if (currentTime < 5) {
-      const currentChar = roadCopy[currentTime]
+  while (currTime < time) {
+    if (currTime < 5) {
+      const currentChar = roadCopy[currTime]
       if (currentChar === '|') {
-        const addedAns = new Array(Math.min(5, time) - currentTime + 1).fill(ans.at(-1))
-        waitTime = addedAns.length - 1
-        ans.splice(ans.length - 1, 1, ...addedAns)
-        currentTime = 5
+        const plus = new Array(Math.min(5, time) - currTime + 1)
+        plus.fill(ans.at(-1))
+        waitTime = plus.length - 1
+        ans.splice(ans.length - 1, 1, ...plus)
+        currTime = 5
         roadCopy = [...road.replace('S', '.').replaceAll('|', '*')]
         continue
       } else {
         const currentStep = [...roadCopy]
-        currentStep[currentTime] = 'S'
+        currentStep[currTime] = 'S'
         ans.push(currentStep.join(''))
-        currentTime++
+        currTime++
       }
     } else {
       const currentStep = [...roadCopy]
-      currentStep[currentTime - waitTime] = 'S'
+      currentStep[currTime - waitTime] = 'S'
       ans.push(currentStep.join(''))
-      currentTime++
+      currTime++
     }
   }
   return ans
