3010c0bee1d85e2d92f3a02055e38fd80c87f97b
jamerrq <jamerrq@gmail.com>
2023-12-22 14:34:16 -0500

    🚂 refactored challenge 22: programming language

--- src/challenges/22.ts
+++ src/challenges/22.ts
@@ -5,23 +5,21 @@ export function compile (code: string): number {
   let inside = null
   const instructions = code.split('')
   while (index < instructions.length) {
+    const outside = inside !== false
     const instruction = instructions[index]
-    if (instruction === '+') {
-      ans += 1 * +(inside === null || inside)
-    } else if (instruction === '-') {
-      ans -= 1 * +(inside === null || inside)
-    } else if (instruction === '*') {
-      ans *= 2 * +(inside === null || inside)
-    } else if (instruction === '%') {
-      last = [last, index][+(inside === null || inside)]
-    } else if (instruction === '<') {
-      instructions[index] = ' '
-      index = [last - 1, index - 1][+(last === -1)]
-    }
+    ans += +(outside) * +(instruction === '+')
+    ans -= 1 * +(outside) * +(instruction === '-')
+    ans *= [1, 2][+(outside) * +(instruction === '*')]
+    last = [last, index][+(outside) * +(instruction === '%')]
     const a = +(instruction === '¿')
     const b = +(ans > 0)
     const c = +(instruction === '?')
-    inside = [inside, null, true, false][2 * a + c + (1 - b) * (1 - c) * (a + b)]
+    inside = [inside, null, true, false][
+      2 * a + c + (1 - b) * (1 - c) * (a + b)
+    ]
+    const prev = index
+    index = [index, last - 1][+(last !== -1) * +(instructions[index] === '<')]
+    instructions[prev] = [instructions[index], ' '][+(instruction === '<')]
     index++
   }
   return ans
